6f0ddfe4c303bd479674620a85014915e28bf65d
valer <valer@HP-PC.homenet.telecomitalia.it>
2020-03-30 00:22:30 +0200

    aggiunto logging lato client

--- progetti-test/demo-sb/src/main/java/com/example/demo/DemoApplication.java
+++ progetti-test/demo-sb/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import com.example.demo.model.PersonaModel;
 
@@ -21,5 +22,14 @@ public class DemoApplication {
 		persona.setCf("XXXXXXXXXXXXX");
 		return persona;
 	}
-
+	
+	@Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }
 }
--- progetti-test/demo-sb/src/test/java/com/example/demo/DemoApplicationTests.java
+++ progetti-test/demo-sb/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 
+import javax.annotation.PostConstruct;
+
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -16,48 +20,67 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.xmlunit.matchers.CompareMatcher;
+
+import com.example.demo.model.PersonaModel;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations="classpath:application-test.properties")
 public class DemoApplicationTests {
+	@Value("${url.base}")
+	private String baseUrl;
+	
 	@LocalServerPort
 	private int port;
+	
+	private String url;
+	
+	@PostConstruct
+	public void init() {
+		url=baseUrl+":"+port;
+		//UriComponentsBuilder.fromHttpUrl("{url.base}:${local.server.port}/demo/persona").build().encode().toUri();
+	    restTemplate.getRestTemplate().setRequestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+		restTemplate.getRestTemplate().getInterceptors().add(new RequestResponseLoggingInterceptor());
+	}
+	
 
-	TestRestTemplate restTemplate = new TestRestTemplate();
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void testJSON() throws JSONException {
 		HttpHeaders headers = new HttpHeaders();
-
-		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-		ResponseEntity<String> response = restTemplate.exchange(
-				createURLWithPort("/demo/persona"),
-				HttpMethod.GET, entity, String.class);
-
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<String> response = restTemplate.exchange(url+"/demo/persona",HttpMethod.GET,entity,String.class);
 		String expected = "{nome:pippo,cognome:baudo,cf:XXXXXXXXXXXXX}";
-
 		JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
 	@Test
 	public void testXML() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
-		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-		ResponseEntity<String> response = restTemplate.exchange(
-				createURLWithPort("/demo/persona"),
-				HttpMethod.GET, entity, String.class);
-
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<String> response = restTemplate.exchange(url+"/demo/persona",HttpMethod.GET, entity, String.class);
 		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><persona><cf>XXXXXXXXXXXXX</cf><cognome>baudo</cognome><nome>pippo</nome></persona>";
 		assertThat(expected, CompareMatcher.isIdenticalTo(response.getBody()));
-		
 	}
 	
-	private String createURLWithPort(String uri) {
-		return "http://localhost:" + port + uri;
+	@Test
+	public void testObject() {
+		PersonaModel response = restTemplate.getForObject(url+"/demo/persona", PersonaModel.class);
+		PersonaModel expected = new PersonaModel();
+		expected.setNome("pippo");
+		expected.setCognome("baudo");
+		expected.setCf("XXXXXXXXXXXXX");	
+		assertThat(expected, CompareMatcher.isIdenticalTo(response));
 	}
+
 
 }
